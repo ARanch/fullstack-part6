58c712f2a578b99228d28e728d6421e6e5492226
Andreas Ranch <ranch@sowasound.com>
2023-02-09 21:54:48 +0100

    6.20 -> 6.22 ✅

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+
+import { fetchAnecdotes, updateAnecdote } from './services/anecdotes'
+
+// todo Implement retrieving anecdotes from the server using React Query.
+// todo Implement adding a new anecdote using React Query.
+// todo Implement voting for an anecdote using React Query.
 
 const App = () => {
+  const queryClient = useQueryClient() // used for adding or updating data in the cache and server
+  const anecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updatedAnecdote) => {
+      queryClient.setQueryData('anecdotes', (oldAnecdotes) => {
+        return oldAnecdotes.map((anecdote) => {
+          if (anecdote.id === updatedAnecdote.id) {
+            return updatedAnecdote
+          }
+          return anecdote
+        })
+      })
+    }
+  })
 
   const handleVote = (anecdote) => {
     console.log('vote')
+    anecdote.votes += 1
+    anecdoteMutation.mutate(anecdote)
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery('anecdotes', fetchAnecdotes)
+
+  if (result.isLoading) {
+    return <div>Loading...</div>
+  } else if (result.isError) {
+    return <div>Error: {result.error.message}</div>
+  }
+
+  console.log(result.data)
+  const anecdotes = result.data
 
   return (
     <div>
@@ -26,9 +51,7 @@ const App = () => {
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
+            , has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,10 +1,28 @@
+import { useMutation, useQueryClient } from 'react-query'
+import { createAnecdote } from '../services/anecdotes'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const anecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      queryClient.setQueryData('anecdotes', (oldAnecdotes) => {
+        return [...oldAnecdotes, newAnecdote]
+      })
+    }
+  }
+  )
+  // const anecdoteMutation = useMutation(
+  //   (newAnecdote) => {
+
+
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    console.log('new anecdote:', content)
+    anecdoteMutation.mutate(content)
+
 }
 
   return (
